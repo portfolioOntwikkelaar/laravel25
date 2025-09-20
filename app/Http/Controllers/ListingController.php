867f0f5253a862165aafd3e3de58eb880770c88a
WebDeveloper <wesleydpnt@gmail.com>
2025-09-20 18:04:48 +0200

    create

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 
+use Illuminate\Validation\Rule;
+
+
 class ListingController extends Controller
 {
     // Laat alle listings zien
@@ -22,6 +25,31 @@ class ListingController extends Controller
             'listing' => $listing
         ]);
     }
+    //Shoze create form
+    public function create() {
+        return view('listings.create');
 }
+    //Store listing data
+    public function store(Request $request) {
+        //dd($request->all());
 
+        $formFields = $request->validate([
+            'company' => ['required', Rule::unique('listings','company')],
+            'title' => ['required', 'min:3'],
+            'location' => 'required',
+            'website' => 'required',
+            'email' => ['required', 'email'],
+            'tags' => 'required',
+            'description' => ['required', 'min:10']
+        ]);
+
+        if($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        Listing::create($formFields);
+
+        return redirect('/')->with('message', 'Listing created successfully!');
+    }
+}   
 
--- routes/web.php
+++ routes/web.php
@@ -6,27 +6,21 @@ use App\Http\Controllers\ListingController;
 // ✅ Alle listings tonen
 Route::get('/listings', [ListingController::class, 'index']);
 
-// ✅ Eén specifieke listing tonen
-Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
 
 // ✅ Home route (je kan dit eventueel ook via controller doen later)
 Route::get('/', function () {
     
     return view('listings.index', [
         'heading' => 'Latest Listings',
-        'listings' => \App\Models\Listing::latest()->filter(request(['tag']))->get()
+        'listings' => \App\Models\Listing::latest()->filter(request(['tag', 'search']))->get()
     ]);
 });
 
-// Route::get('/listings/{id}', function ($id) {
-//     $listing = Listing::find($id);
+Route::get('/listings/create', [ListingController::class, 'create']);
 
-//     if($listing) {
-//     return view('listings', [
-        
-//         'listing' => $listing
-//     ]);
-//     } else {
-//         abort('404');
-//     }
-// });
+// ✅ store listing data
+Route::post('/listings', [ListingController::class, 'store']);
+
+// ✅ Eén specifieke listing tonen
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
